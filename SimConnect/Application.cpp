61f71ae8963d884dac828d481f06e9461bb35db3
ngomez <naimgomezcn@gmail.com>
2021-11-13 22:00:46 +0100

    dispatchProc working working with UUID

--- SimConnect/Application.cpp
+++ SimConnect/Application.cpp
@@ -16,27 +16,14 @@
 #include "SimConnect.h"
 #include <iostream>
 
-
-
 int     quit = 0;
 HANDLE  hSimConnect = NULL;
-
-struct Struct1
-{
-	//double  left_gear;
-	//double  brakes;
-	//double  heading;
-	double  throttle1;
-};
-
-static enum EVENT_ID {
-	EVENT_SIM_START,
-};
+#define BASE_DATA_ID 1000
+typedef bool SETTABLE;
 
 static enum DATA_DEFINITION_ID {
 	DEFINITION_1,
-	THROTTLE1,
-	THROTTLE2,
+	DEFINITION_2,
 };
 
 static enum DATA_REQUEST_ID {
@@ -44,11 +31,11 @@ static enum DATA_REQUEST_ID {
 	REQUEST_2,
 };
 
-/*
-	Custom Easy SimConnect types
-*/
-typedef bool SETTABLE;
 
+static enum DATA_NAMES {
+	DATANAME1,
+	DATANAME2,
+};
 
 /*
 	Main manager of the simvars
@@ -56,7 +43,7 @@ typedef bool SETTABLE;
 class Data
 {
 public:
-	int Id; // Unique global identifier for SimVar
+	int ID; // Unique global identifier for SimVar
 
 private:
 	HANDLE  hSimConnect;
@@ -64,6 +51,7 @@ private:
 	const char* DatumName;			// Name of the MFS2020 simulation variable. Docs: file:///C:/MSFS%20SDK/Documentation/html/Programming_Tools/SimVars/Simulation_Variables.htm
 	const char* UnitsName;			// Specifies the units of the variable.     Docs: file:///C:/MSFS%20SDK/Documentation/html/Programming_Tools/SimVars/Simulation_Variable_Units.htm
 	SIMCONNECT_DATATYPE DatumType;  // Data type of the variable, specification by SDK Docs: ???
+	DATA_NAMES DataName;
 	SETTABLE isSettable;            // A bool that indicated is variable can be modified		
 	
 
@@ -73,11 +61,16 @@ public:
 		DATA_DEFINITION_ID  _DefineID,
 		const char* _DatumName,
 		const char* _UnitsName,
-		SETTABLE _isSettable,
-		SIMCONNECT_DATATYPE _DatumType = SIMCONNECT_DATATYPE_FLOAT64
+		SIMCONNECT_DATATYPE _DatumType,
+		SETTABLE _isSettable
 	) : hSimConnect(_hSimConnect), DefineID(_DefineID), DatumName(_DatumName), UnitsName(_UnitsName), isSettable(_isSettable), DatumType(_DatumType)
 	{
-		SimConnect_AddToDataDefinition(hSimConnect, _DefineID, _DatumName, _UnitsName);
+
+		static int _id = BASE_DATA_ID;
+		ID = _id++;
+		SimConnect_AddToDataDefinition(hSimConnect, _DefineID, _DatumName, _UnitsName, DatumType, 0, ID);
+		std::cout << "DATANAME1: " << DATANAME2 << std::endl;
+		std::cout << "new sim object: " << _DatumName << " - id: " << ID << std::endl;
 	}
 
 	template <typename T>
@@ -127,8 +120,6 @@ struct StructDatum {
 	StructOneDatum  datum[maxReturnedItems];
 };
 
-
-
 void CALLBACK MyDispatchProcPDR(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext)
 {
 	HRESULT hr;
@@ -142,25 +133,41 @@ void CALLBACK MyDispatchProcPDR(SIMCONNECT_RECV* pData, DWORD cbData, void* pCon
 
 		switch (pObjData->dwRequestID)
 		{
-		case DEFINITION_1:
+		/*case DEFINITION_1:
 		{
+
 			int	count = 0;;
 			StructDatum* pS = (StructDatum*)&pObjData->dwData;
-
-			// There can be a minimum of 1 and a maximum of maxReturnedItems
-			// in the StructDatum structure. The actual number returned will
-			// be held in the dwDefineCount parameter.
-
 			while (count < (int)pObjData->dwDefineCount)
 			{
 				switch (pS->datum[count].id)
 				{
-				case THROTTLE1:
+				case DATANAME1:
+					printf("\nTHROTTLE1 = %f", pS->datum[count].value);
+					break;									
+				default:
+					break;
+				}
+				++count;
+			}
+			break;
+		}*/
+		case DEFINITION_1:
+		{
+			int	count = 0;;
+			StructDatum* pS = (StructDatum*)&pObjData->dwData;
+			while (count < (int)pObjData->dwDefineCount)
+			{
+
+				switch (pS->datum[count].id)
+				{
+				case 1000:
 					printf("\nTHROTTLE1 = %f", pS->datum[count].value);
 					break;
-
+				case 1001:
+					printf("\nTHROTTLE2 = %f", pS->datum[count].value);
+					break;
 				default:
-					//printf("\nUnknown datum ID: %d", pS->datum[count].id);
 					break;
 				}
 				++count;
@@ -195,13 +202,18 @@ void initSimConnect()
 		//hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "GEAR LEFT POSITION", "percent");
 		//hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "BRAKE INDICATOR", "Position");
 		//hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE HEADING DEGREES GYRO", "Degrees");
-		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "GENERAL ENG THROTTLE LEVER POSITION:1", "Bool", SIMCONNECT_DATATYPE_FLOAT32, 0 , THROTTLE1);
+		//hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "GENERAL ENG THROTTLE LEVER POSITION:1", "Bool", SIMCONNECT_DATATYPE_FLOAT32, 0 , DATANAME1);
 
-		//Data throttle1(hSimConnect, THROTTLE1, "GENERAL ENG THROTTLE LEVER POSITION:1", "percent", true);
+		Data throttle1(hSimConnect, DEFINITION_1, "GENERAL ENG THROTTLE LEVER POSITION:1", "percent", SIMCONNECT_DATATYPE_FLOAT32, true);
+		Data throttle2(hSimConnect, DEFINITION_1, "GENERAL ENG THROTTLE LEVER POSITION:2", "percent", SIMCONNECT_DATATYPE_FLOAT32, true);
+
+		
+
+		//hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "GENERAL ENG THROTTLE LEVER POSITION:1", "Bool", SIMCONNECT_DATATYPE_FLOAT32, 0, DATANAME1);
+		hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_1, DEFINITION_1, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SIM_FRAME, SIMCONNECT_DATA_REQUEST_FLAG_CHANGED | SIMCONNECT_DATA_REQUEST_FLAG_TAGGED);
 		//hr = SimConnect_AddToDataDefinition(hSimConnect, 0, "GENERAL ENG THROTTLE LEVER POSITION:1", "percent", SIMCONNECT_DATATYPE_FLOAT64);
 		
 		//hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_1, DEFINITION_1, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SIM_FRAME);
-		hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_1, DEFINITION_1, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SIM_FRAME, SIMCONNECT_DATA_REQUEST_FLAG_CHANGED | SIMCONNECT_DATA_REQUEST_FLAG_TAGGED);
 
 		//hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_2, THROTTLE1, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SIM_FRAME);
 
@@ -215,34 +227,12 @@ void initSimConnect()
 	}
 }
 
-class identif
-{
-
-};
-
-class person
-{
-public:
-	int _id;
-
-	person(int a)
-	{
-		static int id = 600; _id = id++;
-
-		std::cout << _id << std::endl;
-	}
-
-};
 
 int main()
 {
-	//initSimConnect();
+	initSimConnect();
 
-	person a(1);
-	person b(1);
-	person ca(1);
-	person aa(1);
-	person agg(1);
+	
 
 	//Settable flap(777);
 	//myMax(1, 1);
